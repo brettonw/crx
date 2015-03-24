08e2cda7e904a219f4dbcb5806f272581f4084f8
Bretton Wade <Bretton.Wade@gmail.com>
2015-03-24 03:42:28 -0400

    add some more test cases for particles

---
+++ src/test/javascript/gravityparticle.js
@@ -0,0 +1,25 @@
+TestContainer.addGame("Gravity Particle", function (ship) {
+        var particle;
+        return {
+            "setup": function (container) {
+                // create the particle
+                particle = Manager.addParticle(function () {
+                    var r = 0.01, d = 300;
+                    return Object.create(Particle).init("Particle", Vector2d.zero(), r, d).
+                        makeGeometry(container);
+                }());
+
+                // gravity toward the pointer
+                Manager.setGravity(function (particle, deltaTime) {
+                    var gravityVector = GameKeys.targetPt.subtract(particle.position);
+                    particle.applyAcceleration(gravityVector);
+                });
+            },
+
+            "play": function () {
+                // update the particle
+                particle.paint();
+            },
+            "finish": function () { }
+        };
+    }());
---
+++ src/test/javascript/simpleparticle.js
@@ -0,0 +1,33 @@
+TestContainer.addGame("Simple Particle", function (ship) {
+        var particle;
+        return {
+            "setup": function (container) {
+                // create the particle
+                particle = Manager.addParticle(function () {
+                    var r = 0.01, d = 300;
+                    return Object.create(Particle).init("Particle", Vector2d.zero(), r, d).
+                        makeGeometry(container);
+                }());
+            },
+
+            "play": function () {
+                // update the particle
+                particle.paint();
+
+                // do something to the particle to move it around
+                if (GameKeys.isDown(GameKeys.codes.rightArrow)) {
+                    particle.applyAcceleration(Vector2d.xy (1, 0));
+                }
+                if (GameKeys.isDown(GameKeys.codes.leftArrow)) {
+                    particle.applyAcceleration(Vector2d.xy(-1, 0));
+                }
+                if (GameKeys.isDown(GameKeys.codes.upArrow)) {
+                    particle.applyAcceleration(Vector2d.xy(0, 1));
+                }
+                if (GameKeys.isDown(GameKeys.codes.downArrow)) {
+                    particle.applyAcceleration(Vector2d.xy(0, -1));
+                }
+            },
+            "finish": function () { }
+        };
+    }());
